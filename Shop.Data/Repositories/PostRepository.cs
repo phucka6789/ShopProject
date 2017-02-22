b005d99701c9e20d652405e303fc6b704ccaaa88
DESKTOP-GORAGGV\PhucTrinh <tdphuc10@gmail.com>
2017-02-22 21:34:18 +0700

    add Repository for

---
+++ Shop.Data/Repositories/PostRepository.cs
@@ -0,0 +1,21 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Repositories
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+    }
+
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
+    {
+        public PostRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ Shop.Data/Repositories/SlideRepository.cs
@@ -0,0 +1,21 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Repositories
+{
+    public interface ISlideRepository : IRepository<Slide>
+    {
+    }
+
+    public class SlideRepository : RepositoryBase<Slide>, ISlideRepository
+    {
+        public SlideRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
